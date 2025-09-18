6d0883f07ebfa1cd7af2e7d548c7348fa23be80b
Mirko <mario.haunold07@gmail.com>
2025-09-18 10:43:09 +0200

    Update platformio.ini and main.cpp for ESP32-S3 configuration and initialization

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
- void setup(){
 
- }
+void setup(){
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("Hello World!");
+}
 
- void loop(){
+void loop(){
     
- }
+}
